7ae1f681ff0298ed5c5864ada07215f25f75d8c9
yogimaveric <109497025+yogimaveric@users.noreply.github.com>
2022-09-07 19:50:53 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component, ElementRef, VERSION, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDatepicker } from '@angular/material/datepicker';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  @ViewChild('datepickerFooter', { static: false })
+  datepickerFooter!: ElementRef;
+  @ViewChild('datepickerFooter1', { static: false })
+  datepickerFooter1!: ElementRef;
+  @ViewChild('picker', { static: false })
+  picker!: MatDatepicker<any>;
+  startDate: Date | null = null;
+  endDate: Date | null = null;
+  selectedValue: Date | null = null;
   name = 'Angular ' + VERSION.major;
+
+  today() {
+    this.startDate = new Date();
+    this.endDate = new Date();
+    this.picker.close();
+  }
+  yestarday() {
+    let today = new Date();
+    this.startDate = new Date(today.setDate(today.getDate() - 1));
+    this.endDate = new Date();
+    this.picker.close();
+  }
+  lmonth() {
+    let today = new Date();
+    let lastmonthstart = new Date(today.getFullYear(), today.getMonth() - 1, 1);
+    let lastmonthend = new Date(today.getFullYear(), today.getMonth(), 0);
+    this.startDate = lastmonthstart;
+    this.endDate = lastmonthend;
+    this.picker.close();
+  }
+  thismonth() {
+    let today = new Date();
+    let currentmonthstart = new Date(today.getFullYear(), today.getMonth(), 1);
+    let currentmonthend = new Date(
+      today.getFullYear(),
+      today.getMonth() + 1,
+      0
+    );
+    this.startDate = currentmonthstart;
+    this.endDate = currentmonthend;
+    this.picker.close();
+  }
+  last_7_days() {
+    let today = new Date();
+    this.startDate = new Date(today.setDate(today.getDate() - 7));
+    this.endDate = new Date();
+    this.picker.close();
+  }
+  onOpen() {
+    this.appendFooter();
+  }
+  cancel() {
+    this.picker.close();
+  }
+  private appendFooter() {
+    const matCalendar = document.getElementsByClassName(
+      'mat-datepicker-content'
+    )[0] as HTMLElement;
+    matCalendar.appendChild(this.datepickerFooter.nativeElement);
+    matCalendar.appendChild(this.datepickerFooter1.nativeElement);
+  }
+  submit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { MatListModule } from '@angular/material/list';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatListModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
